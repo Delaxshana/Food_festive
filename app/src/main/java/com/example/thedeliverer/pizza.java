6a926b9abbd593827198dc3da9854066d18eb2b4
delaxshana <delaxshanas14@gmail.com>
2020-08-11 19:05:06 +0530

    UI created

--- app/src/main/java/com/example/thedeliverer/pizza.java
+++ app/src/main/java/com/example/thedeliverer/pizza.java
@@ -2,21 +2,13 @@ package com.example.thedeliverer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class pizza extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pizza);
     }
-    public void navigationOne(View view){
-        Intent intent;
-        intent = new Intent(this, Login.class);
-        startActivity(intent);
-    }
-
 }
