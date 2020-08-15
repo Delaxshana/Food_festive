6cf76464470181420056fc98fa0d26b1b0b62126
delaxshana <delaxshanas14@gmail.com>
2020-08-15 10:43:42 +0530

    UI updated

--- app/src/main/java/com/example/thedeliverer/MainActivity.java
+++ app/src/main/java/com/example/thedeliverer/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pizza);
+        setContentView(R.layout.activity_dashboard);
     }
     public void navigationOne(View view){
         Intent intent;
