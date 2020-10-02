3415d2f8911ca65178ed6be0ec685d1124ac6b29
IT19085968 <it19085968@my.sliit.lk>
2020-10-02 07:56:50 +0530

    Made connectivity

--- app/src/main/java/com/example/thedeliverer/FoodCart.java
+++ app/src/main/java/com/example/thedeliverer/FoodCart.java
@@ -2,7 +2,10 @@ package com.example.thedeliverer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FoodCart extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class FoodCart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_cart);
+
+        Button btnCont = (Button) this.findViewById(R.id.btn_continue);
+
+        btnCont.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(FoodCart.this,OrderDetails.class);
+                startActivity(i1);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/thedeliverer/OrderDetails.java
+++ app/src/main/java/com/example/thedeliverer/OrderDetails.java
@@ -2,7 +2,10 @@ package com.example.thedeliverer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OrderDetails extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class OrderDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_details);
+
+        Button continuebtn = (Button) this.findViewById(R.id.continue_button);
+
+        continuebtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i2 = new Intent(OrderDetails.this,OrderConfirmation.class);
+                startActivity(i2);
+            }
+        });
     }
 }
